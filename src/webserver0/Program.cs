b84e7008d8d09cc60cffb9d0343ee7cdf95384a9
minhtaile2712 <minhtaile2712@gmail.com>
2023-10-02 18:20:26 +0700

    ok otel

--- src/webserver0/Program.cs
+++ src/webserver0/Program.cs
@@ -1,8 +1,33 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var otlpEndpoint = builder.Configuration.GetValue<string>("OtlpExporter:Endpoint");
+var applicationName = builder.Environment.ApplicationName;
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(b => b
+        .AddService(serviceName: applicationName))
+    .WithTracing(b => b
+        .AddAspNetCoreInstrumentation()
+        .AddEntityFrameworkCoreInstrumentation()
+        .AddGrpcClientInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(opt =>
+        {
+            if (otlpEndpoint != null)
+                opt.Endpoint = new Uri(otlpEndpoint);
+        }))
+    .WithMetrics(b => b
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter());
 
 var app = builder.Build();
 
